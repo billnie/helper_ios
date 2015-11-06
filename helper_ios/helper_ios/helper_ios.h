12285acd539985cb2302f6415595954b8127213e
billnie <706919534@qq.com>
2015-11-06 17:17:57 +0800

    add file

---
+++ helper_ios/helper_ios/helper_ios.h
@@ -0,0 +1,19 @@
+//
+//  helper_ios.h
+//  helper_ios
+//
+//  Created by billnie on 11/6/15.
+//  Copyright © 2015 billnie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for helper_ios.
+FOUNDATION_EXPORT double helper_iosVersionNumber;
+
+//! Project version string for helper_ios.
+FOUNDATION_EXPORT const unsigned char helper_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <helper_ios/PublicHeader.h>
+
+
